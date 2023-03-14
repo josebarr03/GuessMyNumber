54fa289ffaaf437d7cd4676e6e6dc876c1ec0bdd
Jose-CodeGame <jose.barrios@uniat.edu.mx>
2023-03-14 08:20:57 -0600

    First Commit

---
+++ GuessMyNumber.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<cstdlib> //Trabaja con números random
+using namespace std;
+
+int main()
+{
+
+    srand(static_cast<unsigned int>(time(0)));
+    //srand(time(NULL));
+    int randomNumber = rand();
+    int secretNumber = (randomNumber % 100) + 1;
+    int guess;
+    int tries = 0;
+
+    cout << "GUESS MY NUMBER" << endl;
+    cout << "Adivina el número en el menor número de intentos posible." << endl;
+
+    //Depurar
+    cout << secretNumber;
+    do
+    {
+        cout << "\nEnter a guess (1-100):" << endl;
+        cin >> guess;
+        tries++;
+
+        if (guess > secretNumber) {
+            cout << "Muy Alto\n\n";
+        }
+        else if (guess < secretNumber) {
+            cout << "Muy Bajo\n\n";
+        }
+        else {
+            cout << "Excelente lo hiciste en -- " << tries << "-- intentos";
+
+        }
+    } while (guess != secretNumber);
+}
