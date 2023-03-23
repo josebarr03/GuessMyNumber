4c825115283d225f9576b097bd98258c60bf810f
josebarr03 <jose.barrios@uniat.edu.mx>
2023-03-23 09:17:22 -0600

    Validation

--- GuessMyNumber.cpp
+++ GuessMyNumber.cpp
@@ -2,6 +2,8 @@
 #include<cstdlib> //Trabaja con números random
 using namespace std;
 
+int validation(int a, int b);
+
 int main()
 {
 
@@ -19,19 +21,39 @@ int main()
     cout << secretNumber;
     do
     {
-        cout << "\nEnter a guess (1-400):" << endl;
-        cin >> guess;
+        guess = validation(1, 400);
         tries++;
 
-        if (guess > secretNumber) {
+        if (guess > secretNumber + 3) 
+        {
             cout << "Muy Alto\n\n";
         }
-        else if (guess < secretNumber) {
+        else if (guess < secretNumber - 3)
+        {
             cout << "Muy Bajo\n\n";
         }
-        else {
+        else if (guess < secretNumber) 
+        {
+            cout << "Bajo pero cerca\n\n";
+        }
+        else if (guess > secretNumber)
+        {
+            cout << "Alto pero cerca\n\n";
+        }
+        else 
+        {
             cout << "Excelente lo hiciste en -- " << tries << "-- intentos";
-
         }
     } while (guess != secretNumber);
+}
+
+int validation(int a, int b)
+{
+    int c;
+    do
+    {
+        cout << "Introduzca un numero (1-400)" << endl;
+        cin >> c;
+    } while (c > b or c > a);
+    return c;
 }
